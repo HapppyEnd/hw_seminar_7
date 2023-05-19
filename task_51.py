0bba327baad6f863e379c40a04ac01a79d1fc4d3
Alena Savostyanova <el.savostyanova90@yandex.ru>
2023-05-19 14:53:52 +0300

    Homework seminar 7.

---
+++ task_51.py
@@ -0,0 +1,29 @@
+"""Задача №51.
+Решение в группах
+Напишите функцию same_by(characteristic, objects), которая
+проверяет, все ли объекты имеют одинаковое значение
+некоторой характеристики, и возвращают True, если это так.
+Если значение характеристики для разных объектов
+отличается - то False. Для пустого набора объектов, функция
+должна возвращать True. Аргумент characteristic - это
+функция, которая принимает объект и вычисляет его
+характеристику.
+Ввод: Вывод:
+values = [0, 2, 10, 6] same
+if same_by(lambda x: x % 2, values):
+print(‘same’)
+else:
+print(‘different)
+"""
+
+
+def same_by(characteristic, objects):
+    result = list(map(characteristic, objects))
+    return all([i == result[0] for i in result])
+
+
+values = [0, 2, 10, 6, 8]
+if same_by(lambda x: x % 2, values):
+    print('same')
+else:
+    print('different')
